c4973c962c5a8c3c994215a2a7345badf87d92a1
Jason Berg <mcberg@gmail.com>
2023-09-21 00:09:51 -0400

    basic file structure

--- src/main/java/com/berg/invictus/InvictusApplication.java
+++ src/main/java/com/berg/invictus/InvictusApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvictusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvictusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvictusApplication.class, args);
+    }
 
 }
